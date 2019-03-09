349a95bbb298884124e232aee421dc5e62a90158
dhd <1246009411@qq.com>
2019-03-09 13:13:36 +0800

    /usr/bin/push

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@
     |
     | Here is where you can register web routes for your application. These
     | routes are loaded by the RouteServiceProvider within a group which
-    | contains the "web" middleware group. Now create something great!
+    | contains the 'web' middleware group. Now create something great!
     |
     */
     use Illuminate\Support\Facades\Route;
@@ -21,7 +21,7 @@
         Route::post('/login', 'UserController@login');
 
         Route::get('/laf', 'LAFController@postList');
-        Route::get('/laf/{id}', 'LAFController@post')->where(["id"=>"/^\d+$/"]);
+        Route::get('/laf/{id}', 'LAFController@post')->where(['id'=>'/^\d+$/']);
         Route::post('/search', 'LAFController@search');
 
         Route::group(['middleware' => 'loginCheck'], function () {
@@ -32,13 +32,13 @@
             Route::get('/user/info', 'UserController@getUserInfo');
             Route::post('/user/avatar', 'UserController@saveAvatar');
 
-            Route::match(['get', 'post'], '/mark/{id}', 'LAFController@markPost')->where(["id"=>"/^\d+$/"]);
+            Route::match(['get', 'post'], '/mark/{id}', 'LAFController@markPost')->where(['id'=>'/^\d+$/']);
 
             Route::post('/submit', 'LAFController@submitPost')->middleware('deduplicate');
 
-            Route::post('/update/{id}', 'LAFController@updatePost')->where(["id"=>"/^\d+$/"]);
+            Route::post('/update/{id}', 'LAFController@updatePost')->where(['id'=>'/^\d+$/']);
 
-            Route::match(['get', 'post'], '/finish/{id}', 'LAFController@finishPost')->where(["id"=>"/^\d+$/"]);
+            Route::match(['get', 'post'], '/finish/{id}', 'LAFController@finishPost')->where(['id'=>'/^\d+$/']);
         });
 
 //        Route::group(['middleware' => 'checkInfo'], function () {
